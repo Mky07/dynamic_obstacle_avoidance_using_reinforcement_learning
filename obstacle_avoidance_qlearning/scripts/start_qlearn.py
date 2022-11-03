d6b8f35bd002ecdcb39b3cfe3c768aa540cd7e11
Muharrem Küçükyılmaz <muharrem@patikarobotics.com>
2022-11-03 21:19:25 +0300

    save the model and then continue where you left off

--- obstacle_avoidance_qlearning/scripts/start_qlearn.py
+++ obstacle_avoidance_qlearning/scripts/start_qlearn.py
@@ -11,10 +11,34 @@ import rospkg
 # import our training environment
 import obstacle_avoidance
 import numpy as np
+import pickle
+
+import os
+
+# def defineLogVariables():
+#     return {
+#         "qlearn.q": {},
+#         "current_episode": 0,
+#         "nepisodes": 0   
+#     }
+
 
 if __name__ == '__main__':
 
     rospy.init_node('obstacle_avoidance', anonymous=True, log_level=rospy.WARN)
+    filename = "/home/mky/rl_ws/src/openai_examples_projects/dynamic_obstacle_avoidance_using_reinforcement_learning/models/model4.pkl"
+
+    # log_varaiables = defineLogVariables()
+    log_values = {
+        "qlearn.q": {},
+        "current_episode": 0,
+        "nepisodes": 0,
+        "cumulated_reward": []
+    }
+    
+    if not os.path.exists(filename):
+        with open(filename, 'wb') as f:
+            pickle.dump(log_values, f)     
 
     # Create the Gym environment
     env = gym.make('ObstacleAvoidance-v0')
@@ -46,15 +70,26 @@ if __name__ == '__main__':
                            alpha=Alpha, gamma=Gamma, epsilon=Epsilon)
     initial_epsilon = qlearn.epsilon
 
+    # load qlearn valeus from file
+    with open(filename, 'rb') as f:
+        log_values = pickle.load(f)
+        qlearn.q = log_values['qlearn.q']
+
+    last_episode = log_values['current_episode']
+    
     start_time = time.time()
     highest_reward = 0
-
+    
+    print("q valeus: {}".format(qlearn.q))
+    print("last episode: {}".format(log_values['current_episode']))
+    print("cumulated_reward: {}".format(log_values['cumulated_reward']))
+    
     cumulated_reward_10_episode = []
     mean_val = 0
     # Starts the main training loop: the one about the episodes to do
     for x in range(nepisodes):
-        rospy.logdebug("############### WALL START EPISODE=>" + str(x))
-        rospy.logerr("*****************************************\nq values:\n {}\n*****************************************".format(qlearn.q))
+        rospy.logdebug("############### WALL START EPISODE=>" + str(log_values['current_episode']))
+        # rospy.logerr("*****************************************\nq values:\n {}\n*****************************************".format(qlearn.q))
         cumulated_reward = 0
         done = False
         if qlearn.epsilon > 0.05:
@@ -68,7 +103,7 @@ if __name__ == '__main__':
         # env.render()
         # for each episode, we test the robot for nsteps
         for i in range(nsteps):
-            rospy.logwarn("############### Start" + " episode: " +str(x) +"  step=>" + str(i))
+            rospy.logwarn("############### Start" + " episode: " +str(log_values['current_episode']) +"  step=>" + str(i))
             # Pick an action based on the current state
             action = qlearn.chooseAction(state)
             rospy.logwarn("Next action is:%d", action)
@@ -88,6 +123,7 @@ if __name__ == '__main__':
             rospy.logwarn("# reward that action gave=>" + str(reward))
             rospy.logwarn("# episode cumulated_reward=>" + str(cumulated_reward))
             rospy.logwarn("# State in which we will start next step=>" + str(nextState))
+            
             qlearn.learn(state, action, reward, nextState)
 
             if not (done):
@@ -98,10 +134,14 @@ if __name__ == '__main__':
                 last_time_steps = numpy.append(last_time_steps, [int(i + 1)])
                 break
             rospy.logwarn("############### END Step=>" + str(i))
-            #raw_input("Next Step...PRESS KEY")
-            # rospy.sleep(2.0)
             
-            # rospy.logerr("q values:{}".format(qlearn.q))
+        # save qlearn values to file
+        with open(filename, 'wb') as f:
+            log_values['qlearn.q'] = qlearn.q
+            log_values['current_episode'] = last_episode + x
+            log_values['nepisodes'] = nepisodes
+            log_values['cumulated_reward'].append(cumulated_reward)
+            pickle.dump(log_values, f)
 
         m, s = divmod(int(time.time() - start_time), 60)
         h, m = divmod(m, 60)
@@ -110,11 +150,14 @@ if __name__ == '__main__':
             cumulated_reward) + "     Time: %d:%02d:%02d" % (h, m, s)))
 
         mean_val+= cumulated_reward
-        if x%10 == 0:
-            cumulated_reward_10_episode.append(mean_val/10.0)
-            mean_val = 0
-    
-        print("Cumulated_reward_10_episode: {}".format(cumulated_reward_10_episode))
+        # if x%10 == 0:
+        #     cumulated_reward_10_episode.append(mean_val/10.0)
+        #     mean_val = 0
+        cumulated_reward_10_episode.append(cumulated_reward)
+
+        print("cumulated_reward: {}".format(log_values['cumulated_reward']))
+
+        # print("Cumulated_reward_10_episode: {}".format(cumulated_reward_10_episode))
     
     rospy.loginfo(("\n|" + str(nepisodes) + "|" + str(qlearn.alpha) + "|" + str(qlearn.gamma) + "|" + str(
         initial_epsilon) + "*" + str(epsilon_discount) + "|" + str(highest_reward) + "| PICTURE |"))
